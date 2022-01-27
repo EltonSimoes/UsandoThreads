67ebe6494b8579b0dc81f2586438ce0247e7d757
Elton A. Simões <elton.simoes@sga.pucminas.br>
2022-01-27 16:35:05 -0300

    UsandoThreads_3_4
    Usando Threads 3 e 4

---
+++ UsandoThreads/UsandoThreads_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace UsandoThreads_3
+{
+    /// <summary>
+    /// Agora estamos criando uma instância comum , iniciando uma nova thread,
+    /// onde estamos executando a rotina NovaThread, e, em seguida executamos
+    /// novamente a mesma rotina na mesma instância.
+    /// </summary>
+    class Program
+    {
+        bool ok;
+
+        static void Main(string[] args)
+        {
+            // Cria uma instância comum
+            Program tt = new Program();
+            // Inicia a execução da rotina na novao instância
+            new Thread(tt.NewThread).Start();
+            // Exetuta a rotina na mesma instãncia
+            tt.NewThread();
+        }
+
+        void NewThread()
+        {
+            if (!ok)
+            {
+                ok = true;
+                Console.WriteLine("www.macoratti.net");
+            }
+        }
+    }
+}
---
+++ UsandoThreads/UsandoThreads_4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace UsandoThreads_4
+{
+    /// <summary>
+    /// Uma outra forma de compartilhar dados entre as threads é definir campos estáticos.
+    /// </summary>
+    class Program
+    {
+        static bool ok;
+        static bool ok2;
+        static void Main(string[] args)
+        {
+            new Thread(NEwRotina).Start();
+            NEwRotina();
+
+
+            new Thread(NovaRotina2).Start();
+            NovaRotina2();
+            Console.ReadKey();
+
+            TesteLock();
+            Console.ReadKey();
+        }
+
+        static void NEwRotina()
+        {
+            if (!ok)
+            {
+                ok = true;
+                Console.WriteLine("www.macoratti.net");
+            }
+        }
+
+        /// <summary>
+        /// Executando este código o resultado será o mesmo que o exemplo anterior,
+        /// Se alterarmos a ordem das instruções na rotina NovaRotina();
+        /// O problema é que uma thread pode estar avaliando a instrução if enquanto a outra thread
+        /// pode estar executando a instrução Writeline.
+        /// </summary>
+        static void NovaRotina2()
+        {
+            if (!ok2)
+            {
+                Console.WriteLine("www.macoratti.net");
+                ok2 = true;
+            }
+        }
+
+        /// <summary>
+        /// O lock() que vai bloquear os recursos até que a thread que o esta usando acabe de processá-lo.
+        /// </summary>
+
+        static bool ok3;
+        static readonly object bloqueador = new object();
+
+        static void TesteLock()
+        {
+            new Thread(NovaRotina).Start();
+            NovaRotina();
+            Console.ReadKey();
+        }
+
+        static void NovaRotina()
+        {
+            lock (bloqueador)
+            {
+                if (!ok3)
+                {
+                    Console.WriteLine("www.macoratti.net Lock");
+                    ok3 = true;
+                }
+            }
+        }
+    }
+}
