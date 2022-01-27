5a403eae42dc668623de4395da0af25040ec20a5
Elton A. Simões <elton.simoes@sga.pucminas.br>
2022-01-27 16:36:05 -0300

    UsandoThreads_5
    Usando Threads 5

---
+++ UsandoThreads/UsandoThreads_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace UsandoThreads_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Rotina);
+            // Inicia a thread
+            thread.Start();
+            // Aguarda o término da thread
+            thread.Join();
+            Console.WriteLine("\r\n\r\nA thread terminou a sua execução!");
+            Console.ReadKey();
+        }
+
+        static void Rotina()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("www.thread.com.br");
+            }
+        }
+    }
+}
